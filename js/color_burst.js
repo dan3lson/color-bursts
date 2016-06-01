d2a723c101eab22eb911b775d57667d68d4ebbaf
Danelson Rosa <danelson.rosa@gmail.com>
2016-06-01 11:28:16 -0400

    Updates code from February 2015 vanilla JS to June 2016 jQuery.

--- js/color_burst.js
+++ js/color_burst.js
@@ -1,146 +1,149 @@
-// Capture the count of red circles clicked
-var red_circle_count = 0;
-var lives = 3;
-// Add the new shapes to this container
-var shapeContainer = _("shapeContainer");
+$(document).ready(function() {
+	var redCircleCount = 0;
+	var lives = 3;
+	var $shapeContainer = $("#shape-container");
+	var circleDimensions = "33px";
+	var CORRECTCOLOR = "rgb(255, 19, 19)";
 
-play();
+	$("#easy").click(function() {	startGame(3) });
+	$("#medium").click(function() {	startGame(20) });
+	$("#hard").click(function() {	startGame(40) });
+	$("#impossible").click(function() {	startGame(175) });
 
-function play() {
-	_("easy").onclick=function(){
-		startGame(1);
-	};
+	function startGame(levelOfDifficulty) {
+		$("#introHeader").css("display", "none");
+		$("body").css("background", "#FFF");
+		$("#game-header").css("display", "block");
+		$("#score").html(redCircleCount);
+		$("#lives").html(lives);
+		$("#home-icon").click(reload);
+		createAllShapes(levelOfDifficulty);
+	}
 
-	_("medium").onclick=function(){
-		startGame(20);
-	};
-
-	_("hard").onclick=function(){
-		startGame(30);
-	};
-
-	_("impossible").onclick=function(){
-		startGame(150);
-	};
-}
-
-function startGame(levelOfDifficulty) {
-	_("introHeader").style.display = "none";
-	document.body.style.background = "#FFF";
-	_("gameHeader").style.display = "block";
-	_("score").innerHTML = red_circle_count;
-	_("lives").innerHTML = lives;
-	_("homeIcon").onclick=function(){
-		location.reload();
-	};
-	// Display the shapes at the beginning of the game
-	for (var i = 0; i < levelOfDifficulty; i++) {		
-		setInterval(function(){
-			displayShapes();			
-		}, 300);
-	};
-}
-
-
-
-function displayShapes() {
-	var newShape = document.createElement("div");
-	newShape.id = "newShape";
-
-	shapeContainer.appendChild(newShape);
-	
-	var totalShapes = shapeContainer.getElementsByTagName("div").length;
-	var redShapes = totalShapes - 1;
-			
-	// Create the new shape and give it properties
-		// Shape size 
-	newShape.style.width = "33px";
-	newShape.style.height = "33px";
-		// Roundness
-	newShape.style.borderRadius = "33px";
-		// Color
-			// var shape_colors = ["red", "blue", "brown", "purple", "gold", "black", "green", "pink", "orange", "gray"];
-	var shape_colors = ["rgb(255, 19, 0)", "#007AFF", "#5856D6", "#FFCC00", "#1F1F21", "#4CD964", "#FF4981", "#FF9500", "#BDBEC2"];
-	var random_shape_number = randomRange(9,0);
-	newShape.style.background = shape_colors[random_shape_number];
-		// Random position
-	var position_width = randomRange(window.innerWidth, 0) + "px";
-	var position_height = randomRange(window.innerHeight, 0) + "px";
-	newShape.style.left = position_width;
-	newShape.style.top = position_height;
-	newShape.style.position = "absolute";
-				
-	newShape.onclick=function() {
-		// If a red circle is clicked, increase the count by one and vice versa				
-		if(newShape.style.background == "rgb(255, 19, 0)"){
-			red_circle_count++;
-			_("score").innerHTML = red_circle_count;
-			if (red_circle_count == 7) {
-				_("shapeContainer").style.display = "none";
-				_("gameResult").innerHTML = "You win :)";
-				_("endOfGame").style.display = "block";
-				
-				setInterval(function(){
-					location.reload();
-				},2300);
-			}
-		} else{
-			lives--;
-			_("lives").innerHTML = lives;
-			if (lives == 0) {
-				_("shapeContainer").style.display = "none";
-				_("gameResult").innerHTML = "You lose :(";
-				_("endOfGame").style.display = "block";
-				
-				setInterval(function(){
-					location.reload();
-				},2300);
-			}
+	function createAllShapes(levelOfDifficulty) {
+		for (var i = 0; i < levelOfDifficulty; i++) {
+			setInterval(displayShapes, 300);
 		};
-	
-		console.log("Red circle count", red_circle_count);
-		console.log("Lives: ", lives);
-		
-		var totalShapes = shapeContainer.getElementsByTagName("div");
+	}
 
-		for (var i = 0; i < totalShapes.length; i++) {
-			// Randomize the position of the shape
-			var width = randomRange(window.innerWidth, 0) + "px";
-			var height = randomRange(window.innerHeight, 0) + "px";
-			totalShapes[i].style.left = width;
-			totalShapes[i].style.top = height;
-			totalShapes[i].style.position = "absolute";
-			
-			// var shape_colors = ["red", "blue", "brown", "purple", "gold", "black", "green", "pink", "orange", "gray"];
-			var shape_colors = ["rgb(255, 19, 0)", "#007AFF", "#5856D6", "#FFCC00", "#1F1F21", "#4CD964", "#FF4981", "#FF9500", "#BDBEC2"];
-			var random_shape_number = randomRange(9,0);
-			totalShapes[i].style.background = shape_colors[random_shape_number];
-		};	
+	function displayShapes() {
+		var $newShape = createCircle();
+		$shapeContainer.append($newShape);
+		var totalShapes = $shapeContainer.find("div").length;
+		$newShape.css("left", positionWidth());
+		$newShape.css("top", positionHeight());
+		$newShape.css("position", "absolute");
 
-	};	
-};	
+		$newShape.click(function() {
+			if($newShape.css("background-color") == CORRECTCOLOR) {
+				redCircleCount++;
+				$("#score").html(redCircleCount);
+				determineIfWon(redCircleCount);
+			} else {
+				lives--;
+				$("#lives").html(lives);
+				determineIfLost(lives);
+			};
 
-function displayEndOfGame() {
-	// body...
-}
+			console.log("Red circle count", redCircleCount);
+			console.log("Lives: ", lives);
 
+			var totalShapes = $shapeContainer.find(".circle");
+			console.log("Num circles", totalShapes.length);
+			randomizeColor(totalShapes)
+		});
+	};
 
-function randomRange (x,y) {
-	return Math.floor(Math.random()* (x-y) + y);
-};		
+	function randomizeColor(shapes) {
+		for (var i = 0; i < shapes.length; i++) {
+			$(shapes[i]).css("background", randomColor);
+		}
+	}
 
-function rgbValue() {
-	return randomRange(255,0);
-};
+	function determineIfWon(count) {
+		if (count == 7) {
+			setupResultsDisplay();
+			displayGameResult("You win :)")
+			startOver();
+		}
+	}
 
-function randomRadius() {
-	var borderRadiusOptions = ["0px", "75px"];
-	var randomRadius = borderRadiusOptions[randomRange(2,0)]; 
-	
-	return randomRadius;
-}
+	function determineIfLost(lives) {
+		if (lives == 0) {
+			setupResultsDisplay();
+			displayGameResult("You lose :(");
+			startOver();
+		}
+	}
 
-// Shortcut for retrieving an element's ID
-function _(x) {
-	return document.getElementById(x);
-}
+	function setupResultsDisplay() {
+		$shapeContainer.css("display", "none");
+		$("#end-of-game").css("display", "block");
+	}
+
+	function startOver() {
+		setInterval(reload, 1300);
+	}
+
+	function reload() {
+		location.reload();
+	}
+
+	function positionWidth() {
+		return randomRange(window.innerWidth, 0) + "px";
+	}
+
+	function positionHeight() {
+		return randomRange(window.innerHeight, 0) + "px";
+	}
+
+	function displayGameResult(result) {
+		$("#game-result").html(result);
+	}
+
+	function createCircle() {
+		var $circle = createElement("div", "circle");
+		$circle.css("background", randomColor());
+		$circle.css("borderRadius", circleDimensions);
+		$circle.css("height", circleDimensions);
+		$circle.css("width", circleDimensions);
+		return $circle;
+	}
+
+	function randomColor() {
+		// var shape_colors = [
+		// 	"red",
+		// 	"blue",
+		// 	"brown",
+		// 	"purple",
+		// 	"gold",
+		// 	"black",
+		// 	"green",
+		// 	"pink",
+		// 	"orange",
+		// 	"gray"
+		// ];
+		var shape_colors = [
+			CORRECTCOLOR,
+			"#007AFF",
+			"#5856D6",
+			"#FFCC00",
+			"#1F1F21",
+			"#4CD964",
+			"#FF4981",
+			"#FF9500",
+			"#BDBEC2"
+		];
+		return shape_colors[randomRange(9, 0)];
+	}
+
+	function randomRange (x, y) {
+		return Math.floor(Math.random() * (x - y) + y);
+	};
+
+	function createElement(elem, elem_class, elem_id) {
+		elem_class = elem_class ||  null;
+		elem_id = elem_id || null;
+		return $("<" + elem + ">", { class: elem_class, id: elem_id });
+	}
+});
